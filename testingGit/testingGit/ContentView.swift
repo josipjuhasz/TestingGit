879c945dbf9a401abe8713b7ea33fdbb37e7eaed
josip.juhasz <josip.juhasz@rise-world.com>
2022-09-07 18:19:14 +0200

    Added frame

--- testingGit/testingGit/ContentView.swift
+++ testingGit/testingGit/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
+            .frame(width: 100, height: 100, alignment: .center)
             .padding()
     }
 }

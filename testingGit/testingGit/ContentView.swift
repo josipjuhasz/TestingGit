52d1ef43767575f3233836de636f0957c424ee19
josip.juhasz <josip.juhasz@rise-world.com>
2022-09-07 18:07:09 +0200

    Initial commit

---
+++ testingGit/testingGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testingGit
+//
+//  Created by Josip Juhasz on 07.09.2022..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testingGit/testingGit/testingGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingGitApp.swift
+//  testingGit
+//
+//  Created by Josip Juhasz on 07.09.2022..
+//
+
+import SwiftUI
+
+@main
+struct testingGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
